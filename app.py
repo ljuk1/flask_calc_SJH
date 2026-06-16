5ce158815fddefc4d726cfda98f0991f8a42ff1e
Lukasz <lukasz@Lukaszs-MacBook-Pro.local>
2026-06-16 13:23:00 +0100

    feature: front-end fully wired, DTO created correctly, this commit is where methods are still hardcoded, before refactor that comes next but with an already working form and front-end info collection ==> refactor the methods on the backend to accept the dto created on this commit

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
+from pprint import pprint
 from flask import Flask, render_template, request
-
+from userInputDataClass import ShippingRequest
 
 app = Flask(__name__)
 
@@ -15,6 +16,25 @@ def index():
 ## CALCULATOR PAGE ROUTE
 @app.route("/calculator", methods=["GET", "POST"])
 def calculator():
+    if request.method == "POST":
+        user_input_dto = ShippingRequest(
+        country_to=request.form["country_to"],
+        city_to=request.form["city_to"],
+        street_to=request.form["street_to"],
+        postal_code_to=request.form["postal_code_to"],
+        name_to=request.form["name_to"],
+        weight=float(request.form["weight"]),
+        length=int(request.form["length"]),
+        width=int(request.form["width"]),
+        height=int(request.form["height"]),
+        dhl_is_customs_declarable=request.form["dhl_is_customs_declarable"],
+        dhl_is_next_business_day=request.form["dhl_is_next_business_day"],
+
+        ### the rest of fields are from .env, instantiated to default automatically
+        )
+        pprint(user_input_dto.__dict__)
+
+
     return render_template("calculator.html")
 
 
